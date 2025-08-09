80f736368ce41f04d79d8808ec3619ab7eeda195
habibhajeera-creator <habibhajeera@gmail.com>
2025-08-09 14:06:09 +0530

    test

--- src/main/java/com/example/spring_hello/HelloController.java
+++ src/main/java/com/example/spring_hello/HelloController.java
@@ -8,7 +8,7 @@ public class HelloController {
     
     @GetMapping("/")
     public String hello() {
-        return "hello world from Spring Boot!";
+        return "My name is Mariam!";
     }
     
 }
